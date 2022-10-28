0e19326c472ebadab3266860ef299bcae4b191df
Steven Alvarenga <74988877+Steven02-ZT@users.noreply.github.com>
2022-10-27 22:54:38 -0600

    Initial commit

---
+++ Bird eyes/index.js
@@ -0,0 +1,37 @@
+function _defineProperty(obj, key, value) {if (key in obj) {Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });} else {obj[key] = value;}return obj;} // 4/26/2022 Update: This is not quite an example of parallax by some's standards. This was just a simple and incomplete attempt to learn parallax that I thought no one would notice. I did not anticipate this experimental pen to get so many views and I credit the awesomeness of Andre Benz's photography for that completely. 
+
+
+class App extends React.Component {constructor(...args) {super(...args);_defineProperty(this, "handleMouseMove",
+    e => {
+      const el = document.getElementById("wrapper");
+      const d = el.getBoundingClientRect();
+      let x = e.clientX - (d.left + Math.floor(d.width / 2));
+      let y = e.clientY - (d.top + Math.floor(d.height / 2));
+      // Invert values
+      x = x - x * 2;
+      y = y - y * 2;
+      document.documentElement.style.setProperty("--scale", 1.6);
+      document.documentElement.style.setProperty("--x", x / 2 + "px");
+
+      document.documentElement.style.setProperty("--y", y / 2 + "px");
+    });_defineProperty(this, "handleMouseLeave",
+
+    () => {
+      document.documentElement.style.setProperty("--scale", 1);
+      document.documentElement.style.setProperty("--x", 0);
+      document.documentElement.style.setProperty("--y", 0);
+    });}
+  render() {
+    return (
+      React.createElement("div", {
+        id: "wrapper",
+        onMouseMove: this.handleMouseMove,
+        onClick: this.handleMouseLeave }, 
+
+      React.createElement("img", { id: "image" })));
+
+
+  }}
+
+
+ReactDOM.render(React.createElement(App, null), document.getElementById("root"));
---
+++ Wallpaper-smoot-scroll-3d/index.js
@@ -0,0 +1,13 @@
+gsap.set('.main', {position:'fixed', background:'#000', width:'100%', maxWidth:'100vw', height:'100wh', top:0})
+gsap.set('.scrollDist', {width:'100%', height:'200%'})
+gsap.timeline({scrollTrigger:{trigger:'.scrollDist', start:'top top', end:'bottom bottom', scrub:1}})
+.fromTo('.background', {y:0},{y:-200}, 0)
+.fromTo('.cloud1', {y:100},{y:-800}, 0)
+.fromTo('.cloud2', {y:-150},{y:-500}, 0)
+.fromTo('.cloud3', {y:-50},{y:-650}, 0)
+.fromTo('.marco', {y:-10},{y:-100}, 0)
+.fromTo('.person', {y:150},{y:-250}, 0)
+
+$('#arrowBtn').on('mouseenter', (e)=>{ gsap.to('.arrow', {y:10, duration:0.8, ease:'back.inOut(3)', overwrite:'auto'}); })
+$('#arrowBtn').on('mouseleave', (e)=>{ gsap.to('.arrow', {y:0, duration:0.5, ease:'power3.out', overwrite:'auto'}); })
+$('#arrowBtn').on('click', (e)=>{ gsap.to(window, {scrollTo:innerHeight, duration:1.5, ease:'power1.inOut'}); }) // scrollTo requires the ScrollTo plugin (not to be confused w/ ScrollTrigger)
